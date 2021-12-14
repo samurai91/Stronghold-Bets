84e4c6de66b9055cdb50c67bd2d19681020319f0
samurai91 <stalker_2012@spaces.ru>
2021-12-14 04:39:15 +0300

    предварительно адаптив готов

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
-        jQuery("a.header_menu_link").click(function () {
+        jQuery("a.header_menu_link, a.header_block_text_btn").click(function () {
       elementClick = jQuery(this).attr("href")
       destination = jQuery(elementClick).offset().top - 0;
       jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
       return false;
     });
+
     $('.popup_close').on('click', function (events) {
       event.preventDefault();
       $('.popup').fadeOut();
     });
+
+    function heightBlock(column){
+      var myblock = 0;
+
+      column.each(function(){
+        thisHight = $(this).height();
+        if(thisHight > myblock){
+          myblock = thisHight;
+        }
+      });
+      column.height(myblock);
+    };
+    heightBlock($(".offer_block_wrap"))
   });
 $('form').submit(function(event) {
 	event.preventDefault();
